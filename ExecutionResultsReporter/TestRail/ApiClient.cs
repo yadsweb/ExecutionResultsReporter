1253e2434e58ae9bd731e392b3c5e11ff828a9d5
Yani Dimitrov Stoyanov <yads.web@gmail.com>
2016-08-17 18:28:44 +0300

    Revert "making the second request, send if the first return 429 to be compleatly seperated to avoid stream closed error"
    This reverts commit dfb4c7ff2152b6fb798c8e0dd133266ac18401cd.

--- ExecutionResultsReporter/TestRail/ApiClient.cs
+++ ExecutionResultsReporter/TestRail/ApiClient.cs
@@ -21,99 +21,139 @@ using Newtonsoft.Json.Linq;
 
 namespace ExecutionResultsReporter.TestRail
 {
-    public class ApiClient
-    {
-        private string _mUser;
-        private string _mPassword;
-        private string _mUrl;
+	public class ApiClient
+	{
+		private string _mUser;
+		private string _mPassword;
+		private string _mUrl;
         private readonly ILog _log = LogManager.GetLogger("ApiClient");
 
-        public ApiClient(string baseUrl)
-        {
-            if (!baseUrl.EndsWith("/"))
-            {
-                baseUrl += "/";
-            }
-            _mUrl = baseUrl + "index.php?/api/v2/";
-        }
-
+		public ApiClient(string baseUrl)
+		{
+			if (!baseUrl.EndsWith("/"))
+			{
+				baseUrl += "/";
+			}
+			_mUrl = baseUrl + "index.php?/api/v2/";
+		}
+	    
         /**
-         * Get/Set User
-         *
-         * Returns/sets the user used for authenticating the API requests.
-         */
-        public string User
-        {
-            get { return _mUser; }
-            set { _mUser = value; }
-        }
+		 * Get/Set User
+		 *
+		 * Returns/sets the user used for authenticating the API requests.
+		 */
+		public string User
+		{
+			get { return _mUser; }
+			set { _mUser = value; }
+		}
 
-        /**
-         * Get/Set Password
-         *
-         * Returns/sets the password used for authenticating the API requests.
-         */
-        public string Password
-        {
-            get { return _mPassword; }
-            set { _mPassword = value; }
-        }
+		/**
+		 * Get/Set Password
+		 *
+		 * Returns/sets the password used for authenticating the API requests.
+		 */
+		public string Password
+		{
+			get { return _mPassword; }
+			set { _mPassword = value; }
+		}
 
-        /**
-         * Send Get
-         *
-         * Issues a GET request (read) against the API and returns the result
-         * (as JSON object, i.e. JObject or JArray instance).
-         *
-         * Arguments:
-         *
-         * uri                  The API method to call including parameters
-         *                      (e.g. get_case/1)
-         */
-        public object SendGet(string uri)
-        {
-            return SendRequest("GET", uri, null);
-        }
+		/**
+		 * Send Get
+		 *
+		 * Issues a GET request (read) against the API and returns the result
+		 * (as JSON object, i.e. JObject or JArray instance).
+		 *
+		 * Arguments:
+		 *
+		 * uri                  The API method to call including parameters
+		 *                      (e.g. get_case/1)
+		 */
+		public object SendGet(string uri)
+		{
+			return SendRequest("GET", uri, null);
+		}
 
-        /**
-         * Send POST
-         *
-         * Issues a POST request (write) against the API and returns the result
-         * (as JSON object, i.e. JObject or JArray instance).
-         *
-         * Arguments:
-         *
-         * uri                  The API method to call including parameters
-         *                      (e.g. add_case/1)
-         * data                 The data to submit as part of the request (as
-         *                      serializable object, e.g. a dictionary)
-         */
-        public object SendPost(string uri, object data)
-        {
-            return SendRequest("POST", uri, data);
-        }
+		/**
+		 * Send POST
+		 *
+		 * Issues a POST request (write) against the API and returns the result
+		 * (as JSON object, i.e. JObject or JArray instance).
+		 *
+		 * Arguments:
+		 *
+		 * uri                  The API method to call including parameters
+		 *                      (e.g. add_case/1)
+		 * data                 The data to submit as part of the request (as
+		 *                      serializable object, e.g. a dictionary)
+		 */
+		public object SendPost(string uri, object data)
+		{
+			return SendRequest("POST", uri, data);
+		}
 
-        private object SendRequest(string method, string uri, object data)
-        {
-            HttpWebResponse response;
-            JContainer result;
-            Exception ex = null;
+		private object SendRequest(string method, string uri, object data)
+		{
+			var url = _mUrl + uri;
             var text = "";
-            var request = ConstructRequest(method, uri, data);
-            try
-            {
-                response = (HttpWebResponse)request.GetResponse();
-            }
-            catch (WebException e)
-            {
-                if (e.Response == null)
-                {
-                    throw;
-                }
+            JContainer result;
 
-                response = (HttpWebResponse)e.Response;
-                ex = e;
-            }
+			// Create the request object and set the required HTTP method
+			// (GET/POST) and headers (content type and basic auth).
+			var request = (HttpWebRequest)WebRequest.Create(url);
+		    request.Timeout = 180000;
+			request.ContentType = "application/json";
+			request.Method = method;
+
+			string auth = Convert.ToBase64String(
+				Encoding.ASCII.GetBytes(
+					String.Format(
+						"{0}:{1}",
+						_mUser,
+						_mPassword
+					)
+				)
+			);
+
+			request.Headers.Add("Authorization", "Basic " + auth);
+
+			if (method == "POST")
+			{
+				// Add the POST arguments, if any. We just serialize the passed
+				// data object (i.e. a dictionary) and then add it to the request
+				// body.
+				if (data != null)
+				{
+					byte[] block = Encoding.UTF8.GetBytes( 
+						JsonConvert.SerializeObject(data)
+					);
+					request.GetRequestStream().Write(block, 0, block.Length);
+                    //Console.WriteLine(JsonConvert.SerializeObject(data));
+				}
+			}
+
+			// Execute the actual web request (GET or POST) and record any
+			// occurred errors.
+			Exception ex = null;
+			HttpWebResponse response;
+			try
+			{
+				response = (HttpWebResponse)request.GetResponse();
+			}
+			catch (WebException e)
+			{
+				if (e.Response == null)
+				{
+					throw;
+				}
+
+				response = (HttpWebResponse)e.Response;
+				ex = e;
+			}
+
+			// Read the response body, if any, and deserialize it from JSON.
+
             if (response != null)
             {
                 var responseStream = response.GetResponseStream();
@@ -128,22 +168,28 @@ namespace ExecutionResultsReporter.TestRail
                     text = reader.ReadToEnd();
                 }
             }
+
+			
             _log.Debug("Response body is : " + text);
-            if (text != "")
-            {
-                if (text.StartsWith("["))
-                {
-                    result = JArray.Parse(text);
-                }
-                else
-                {
-                    result = JObject.Parse(text);
-                }
-            }
-            else
-            {
-                result = new JObject();
-            }
+			if (text != "")
+			{
+				if (text.StartsWith("["))
+				{
+					result = JArray.Parse(text);
+				}
+				else 
+				{
+					result = JObject.Parse(text);
+				}
+			}
+			else 
+			{
+				result = new JObject();
+			}
+
+			// Check for any occurred errors and add additional details to
+			// the exception message, if any (e.g. the error message returned
+			// by TestRail).
             if ((int)response.StatusCode == 429)
             {
                 ex = null;
@@ -160,11 +206,9 @@ namespace ExecutionResultsReporter.TestRail
                 }
                 _log.Debug("Didn't contains 'Retry-After' we will sleep the default time interval of 5 seconds.");
                 Thread.Sleep(timeToSleep);
-                var request2 = ConstructRequest(method, uri, data);
-                HttpWebResponse secondResponse;
                 try
                 {
-                    secondResponse = (HttpWebResponse)request2.GetResponse();
+                    response = (HttpWebResponse)request.GetResponse();
                 }
                 catch (WebException e)
                 {
@@ -173,12 +217,12 @@ namespace ExecutionResultsReporter.TestRail
                         throw;
                     }
 
-                    secondResponse = (HttpWebResponse)e.Response;
+                    response = (HttpWebResponse)e.Response;
                     ex = e;
                 }
-                if (secondResponse != null)
+                if (response != null)
                 {
-                    var responseStream = secondResponse.GetResponseStream();
+                    var responseStream = response.GetResponseStream();
                     if (responseStream == null)
                     {
                         throw new Exception("Response stream from response is '" + response + "' null!");
@@ -207,34 +251,30 @@ namespace ExecutionResultsReporter.TestRail
                     result = new JObject();
                 }
             }
-            if (ex == null) return result;
-            var error = (string)result["error"];
-            if (error != null)
-            {
-                error = '"' + error + '"';
-            }
-            else
-            {
-                error = "No additional error message received";
-            }
-            throw new APIException(String.Format("TestRail API returned HTTP {0} ({1})", (int)response.StatusCode, error));
-        }
 
-        private HttpWebRequest ConstructRequest(string method, string uri, object data)
-        {
-            var url = _mUrl + uri;
-            var request = (HttpWebRequest)WebRequest.Create(url);
-            var auth = Convert.ToBase64String(Encoding.ASCII.GetBytes(String.Format("{0}:{1}", _mUser, _mPassword)));
-            request.Timeout = 180000;
-            request.ContentType = "application/json";
-            request.Method = method;
-            request.Headers.Add("Authorization", "Basic " + auth);
-            if (method != "POST") return request;
-            if (data == null) return request;
-            var block = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data));
-            request.GetRequestStream().Write(block, 0, block.Length);
-            return request;
-        }
-    }
+			if (ex != null)
+			{
+				var error = (string) result["error"];
+				if (error != null)
+				{
+					error = '"' + error + '"';
+				}
+				else
+				{
+					error = "No additional error message received";
+				}
+
+				throw new APIException(
+					String.Format(
+						"TestRail API returned HTTP {0} ({1})",
+						(int)response.StatusCode,
+						error
+					)
+				);
+			}
+
+			return result;
+		}
+	}
 }
 
